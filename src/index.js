2837b216e26fb8a28f365df6bd1461aec8666ae6
yurishe0 <yurishe00@gmail.com>
2024-03-25 21:32:11 +0100

    fix: include the new css card file

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import './styles/header.css';
 import './styles/content.css';
 import './styles/footer.css';
 import './styles/animations.css';
+import './styles/card.css';
 
 import './assets/flowers.jpg';
