1a7e1bf18565287de8d3602099b32d91cfd3a10f
yurishe0 <yurishe00@gmail.com>
2024-03-25 19:30:58 +0100

    feat: create basic site layout and header design

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './styles/main.css';
+import './styles/header.css';
+import './styles/content.css';
+import './styles/footer.css';
+import './styles/animations.css';
+
+import './assets/flowers.jpg';
