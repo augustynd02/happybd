8d0c82ae5dc12f441857c43606dda77483c772e6
yurishe0 <yurishe00@gmail.com>
2024-03-26 19:31:09 +0100

    refactor: create the circle in html and adjust the styling instead

--- src/ui/hiddenPresent.js
+++ src/ui/hiddenPresent.js
@@ -1,17 +1,10 @@
 const hiddenContainer = document.querySelector('.hidden');
+const circle = document.querySelector('.circle');
 
 hiddenContainer.addEventListener("mousemove", (e) => {
-    const existingCircle = hiddenContainer.querySelector('.circle');
-    if (existingCircle) {
-        hiddenContainer.removeChild(existingCircle);
-    }
     const x = e.clientX;
     const y= e.clientY;
     const containerRect = hiddenContainer.getBoundingClientRect();
-
-    const circle = document.createElement('div');
-    circle.classList.add('circle');
     circle.style.top = y - containerRect.top + 'px';
     circle.style.left = x + 'px';
-    hiddenContainer.append(circle);
 })
