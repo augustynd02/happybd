b04506d4a3e3b04d785b0cf9989a19f29c5613bf
yurishe0 <yurishe00@gmail.com>
2024-03-28 21:31:53 +0100

    feat: add basic gallery with dummy images

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import './styles/footer.css';
 import './styles/animations.css';
 import './styles/card.css';
 import './styles/hiddenpresent.css'
+import './styles/gallery.css'
 
 import './assets/flowers.jpg';
 
 import './ui/rotateCard';
 import './ui/hiddenPresent';
+import './ui/gallery';
 import Confetti from './ui/confetti';
 
 const header = document.querySelector("header");
