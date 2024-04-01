e01812490f5513895fbc01fec7203326b8b30512
yurishe0 <yurishe00@gmail.com>
2024-04-01 12:08:41 +0200

    fix: correctly copy the assets directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
-        assetModuleFilename: 'src/assets/[name].[ext]',
+        assetModuleFilename: 'assets/[name].[ext]',
     },
     module: {
         rules: [
@@ -15,7 +16,7 @@ module.exports = {
                 use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
             {
-              test: /\.(png|jpg|gif|svg|eot|ttf|woff)$/,
+              test: /\.(png|jpg|jpeg|gif|svg|eot|ttf|woff)$/,
               type: 'asset/resource'
            }
         ],
@@ -23,6 +24,11 @@ module.exports = {
     plugins: [
         new MiniCssExtractPlugin({
             filename: 'styles.css',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/assets', to: 'assets' },
+            ],
         }),
     ],
 };
